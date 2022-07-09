969b91976a4d8add1ba661c4c7d1069c90416d78
Silvio Costa <101225535+Henrique-92@users.noreply.github.com>
2022-07-09 16:28:28 -0300

    Add files via upload

---
+++ TS-JS/desafio3.js
@@ -0,0 +1,42 @@
+"use strict";
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+let saldoTotal = 0;
+//campoSaldo.innerHTML = 0
+limparSaldo();
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma() {
+    soma.value = " ";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value));
+    });
+}
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+/*function limparCampoSoma() {
+    throw new Error("Function not implemented.");
+}*/
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
